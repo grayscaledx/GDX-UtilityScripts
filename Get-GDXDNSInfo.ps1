0d2f533889a6ed437309894a2e2bbda13d132ec9
gray <agentdeployed@gmail.com>
2013-10-30 18:31:52 -0400

    New Script -- Get-GDXDNSInfo
    Accepts a list of addresses, IP or FQDN, and return all pertinent DNS
    information and if the host is alive at the time of execution.
    
    Work in progress but this version does work.

---
+++ Get-GDXDNSInfo.ps1
@@ -0,0 +1,74 @@
+﻿Function Get-GDXDNSInfo {
+
+    [CmdletBinding()]
+    param(
+        [Parameter(HelpMessage="A target computer IP address, DNS FQDN, or Machine name.  Can be a list of strings.",
+                    ValueFromPipeline=$True,
+                    ValueFromPipelineByPropertyName=$True)]
+        [ValidateScript({foreach ($ComputerName in $_) {$ComputerName.GetType().FullName -eq "System.String"}})]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )    
+
+    BEGIN { $dnsContainer = @() }
+
+    PROCESS {
+
+        foreach ($C in $ComputerName){
+
+            try {
+                
+                $dnsResults = [Net.DNS]::GetHostEntry($C)
+
+                $dnsProps = @{
+                                'HostName'=$dnsResults.HostName;
+                                'AddressList'=$($($dnsResults.AddressList | Select -ExpandProperty IPAddressToString) -join ';');
+                                'IsAlive'=$(Test-Connection $C -Count 2 -Quiet)
+                            }
+
+                $dnsObj = New-Object -TypeName psobject -Property $dnsProps
+                $dnsObj.PSObject.Typenames.Insert(0,'GDX.DNSInfo')
+                $dnsContainer += $dnsObj
+
+                Write-Verbose "Completed processing target host $C"
+
+            }
+
+            <#catch [System.Net.Sockets.SocketException] {
+                
+                $dnsProps = @{
+                                'HostName'="NoHostFound";
+                                'AddressList'=$C;
+                                'IsAlive'=$(Test-Connection $C -Count 2 -Quiet)
+                            }
+
+                $dnsObj = New-Object -TypeName psobject -Property $dnsProps
+                $dnsObj.PSObject.Typenames.Insert(0,'GDX.DNSInfo')
+                $dnsContainer += $dnsObj
+
+            }#>
+            
+            catch {
+            
+                $dnsProps = @{
+                                'HostName'="NoHostFound";
+                                'AddressList'=$C;
+                                'IsAlive'=$(Test-Connection $C -Count 2 -Quiet)
+                            }
+
+                $dnsObj = New-Object -TypeName psobject -Property $dnsProps
+                $dnsObj.PSObject.Typenames.Insert(0,'GDX.DNSInfo')
+                $dnsContainer += $dnsObj
+
+                Write-Verbose "Completed processing target host $C; No hostname found."
+
+            }                      
+        
+        }
+    
+    }
+
+    END { Write-Output $dnsContainer }
+
+    
+
+}
